5c924f8f9afd279ae39ba79518f85c49568a7431
wheatandcat <wheatandcat@gmail.com>
2018-04-29 16:26:14 +0900

    add express

--- backend/functions/lib/index.js
+++ backend/functions/lib/index.js
@@ -1,10 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const graphql_1 = require("graphql");
+// Init express
+const app = express();
+// Construct a schema, using GraphQL schema language
+const schema = graphql_1.buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return "Hello world!";
+    },
+};
+app.use("/", graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+exports.graphql = functions.https.onRequest(app);
 //# sourceMappingURL=index.js.map
--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,8 +1,32 @@
 import * as functions from "firebase-functions"
+import * as express from "express"
+import * as graphqlHTTP from "express-graphql"
+import { buildSchema } from "graphql"
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!")
-})
+// Init express
+const app = express()
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!"
+  },
+}
+
+app.use(
+  "/",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
+
+exports.graphql = functions.https.onRequest(app)
