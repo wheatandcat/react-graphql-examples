f8773ca04a8cb7cad35261f4a1022922704ad80c
wheatandcat <wheatandcat@gmail.com>
2018-05-06 10:59:03 +0900

    init

--- frontend/src/containers/Auth.js
+++ frontend/src/containers/Auth.js
@@ -1,25 +1,22 @@
-import firebase from "firebase"
-import moment from "moment"
+import firebase from "firebase";
+import moment from "moment";
+import { config } from "../config";
 
-const config = {
-  apiKey: "AIzaSyAbDq1R3q6-dpJ9axF0dohrm1I70gzieS8",
-  authDomain: "example-202505.firebaseapp.com",
-}
-firebase.initializeApp(config)
+firebase.initializeApp(config);
 
 export default class Auth {
   signOut = () => {
-    firebase.auth().signOut()
-  }
+    firebase.auth().signOut();
+  };
 
   signedIn = () => {
-    return !!localStorage.getItem("id_token")
-  }
+    return !!localStorage.getItem("id_token");
+  };
 
   setSession = async () => {
-    const idToken = await firebase.auth().currentUser.getIdToken()
+    const idToken = await firebase.auth().currentUser.getIdToken();
 
-    await localStorage.setItem("id_token", idToken)
-    await localStorage.setItem("expiration", moment().unix() + 60 * 60)
-  }
+    await localStorage.setItem("id_token", idToken);
+    await localStorage.setItem("expiration", moment().unix() + 60 * 60);
+  };
 }
