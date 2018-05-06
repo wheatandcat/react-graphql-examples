a30016501a52a9faac2d052533174a6665f498be
wheatandcat <wheatandcat@gmail.com>
2018-05-06 20:09:29 +0900

    init

--- native/README.md
+++ native/README.md
@@ -1,11 +1,8 @@
-# example
+# native
 
-A simple usage example. If you're using redux, take a look at [example-redux](../example-redux).
 
-## Installation - iOS
+## local ios
 
-* In the `example/` folder, run `npm install`
-
-> Make sure you're using npm ver 3. If you normally use npm ver 2 on your system and reluctant to upgrade, you can install [npm 3 alongside 2](https://www.npmjs.com/package/npm3). For more details see https://github.com/wix/react-native-navigation/issues/1
-
-* Open `example/ios/example.xcodeproj` in Xcode and press the play button
+```
+react-native run-ios
+```
--- native/src/components/pages/SignIn/Connected.js
+++ native/src/components/pages/SignIn/Connected.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { Text, View, Button } from "react-native-ui-lib";
+import { Linking } from "react-native";
 
 export default class extends Component {
+  open = () => {
+    Linking.openURL("https://auth-dot-example-202505.appspot.com");
+  };
   render() {
     return (
-      <View>
-        <Text>test</Text>
+      <View useSafeArea>
+        <View centerH>
+          <Button
+            backgroundColor="#30B650"
+            label="Login"
+            labelStyle={{ fontWeight: "600" }}
+            size="small"
+            enableShadow
+            onPress={this.open}
+          />
+        </View>
       </View>
     );
   }
