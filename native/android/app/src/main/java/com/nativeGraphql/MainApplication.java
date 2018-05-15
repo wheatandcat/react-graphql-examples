28526e46a80b71359734fc0c98892d692bcd7a73
“wheatandcat” <wheatandcat@gmail.com>
2018-05-15 22:04:55 +0900

    ios and androis google login

--- native/android/app/src/main/java/com/nativeGraphql/MainApplication.java
+++ native/android/app/src/main/java/com/nativeGraphql/MainApplication.java
@@ -2,12 +2,32 @@ package com.nativeGraphql;
 
 import android.support.annotation.Nullable;
 
+import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.reactnativenavigation.NavigationApplication;
+import com.facebook.soloader.SoLoader;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends NavigationApplication {
+
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+          new RNGoogleSigninPackage()
+      );
+    }
+
+    @Override
+    public void onCreate() {
+      super.onCreate();
+      SoLoader.init(this, /* native exopackage */ false);
+    }
+
     @Override
     public boolean isDebug() {
         return BuildConfig.DEBUG;
@@ -16,7 +36,7 @@ public class MainApplication extends NavigationApplication {
     @Nullable
     @Override
     public List<ReactPackage> createAdditionalReactPackages() {
-        return null;
+        return getPackages();
     }
 
     @Nullable
